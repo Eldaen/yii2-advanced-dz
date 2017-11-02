d0474298639f42b8d2be668cba6d572bd6c0630b
Eldaen <sv_skatch@bk.ru>
2017-11-02 19:19:29 +0300

    db changes

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=dafuq-site',
+            'dsn' => 'mysql:host=localhost;dbname=dafuqsite',
             'username' => 'eldaen',
             'password' => 'kdeQZqDAdJAvfXh24rETd4',
             'charset' => 'utf8',
