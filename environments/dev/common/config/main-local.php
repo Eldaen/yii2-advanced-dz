17c5fcbb08e0eb79109f559f37186214226d541f
Eldaen <sv_skatch@bk.ru>
2017-11-02 19:17:28 +0300

    Yii2-advanced, 1 урок

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '12345',
+            'dsn' => 'mysql:host=localhost;dbname=dafuq-site',
+            'username' => 'eldaen',
+            'password' => 'kdeQZqDAdJAvfXh24rETd4',
             'charset' => 'utf8',
         ],
         'mailer' => [
